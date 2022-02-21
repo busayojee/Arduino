0bd05561cdbbe5f54be3a3a44b40fccf87d5a8a5
busayojee <74312641+busayojee@users.noreply.github.com>
2022-02-21 21:57:38 +0200

    Update sensortest.ino

--- Arduino/sensortest/sensortest.ino
+++ Arduino/sensortest/sensortest.ino
@@ -6,7 +6,7 @@ Serial.begin(9600);
 
 void loop() {
   // put your main code here, to run repeatedly:
-int val =  analogRead(A1);
+int val =  analogRead(A1); // I connected each sensor to A1 on the Arduino uno
 Serial.println(val);
 
 }
